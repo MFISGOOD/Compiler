d6b0d758e7297b71bb6a9843ac6beb596177b87e
MFISGOOD <turbot2@hotmail.com>
2018-02-05 20:43:43 +0100

    the final version | la version finale

--- compiler.js
+++ compiler.js
@@ -20,16 +20,84 @@ Compiler.prototype.pass1 = function (program) {
   return parse(tokens);
 };
 
+function calc(a,op,b){
+  switch(op){
+    case "*" : return parseFloat(a) * parseFloat(b);
+    case "/" : return parseFloat(a) / parseFloat(b);
+    case "+" : return parseFloat(a) + parseFloat(b);
+    case "-" : return parseFloat(a) - parseFloat(b);
+  }
+}
 Compiler.prototype.pass2 = function (ast) {
   // return AST with constant expressions reduced
-  return ast;
+  (function reduce(parent,program,arg){
+      if( program && 'a' in program && 'b' in program)
+      if(program.a.op === 'imm' && program.b.op ==='imm'){
+        let result = calc(program.a.n,program.op,program.b.n);
+        parent[arg] = {op: 'imm',n:result};
+        reduce(ast, ast, "");
+      }else{    
+           reduce(program,program.b,'b');  
+           reduce(program,program.a,'a');           
+      }
+   })(ast,ast);
+   return ast;
 };
 
 Compiler.prototype.pass3 = function (ast) {
   // return assembly instructions
-  return ast;
+  let ins = {"+" : 'AD',"-":'SU',"*":'MU',"/":'DI'};
+  let left = false;
+  let right= false;
+  function func(op){
+    let result = [];
+    if(op.a.op === 'imm'){
+        result.push(`IM ${op.a.n}`);
+        result.push("PU");
+     }
+    else if(op.a.op === 'arg'){
+        result.push(`AR ${op.a.n}`);
+        result.push("PU");
+     }    
+    else{
+           result.push(...func(op.a));
+    } 
+    if(op.b.op === 'imm'){
+        result.push(`IM ${op.b.n}`);
+        result.push("PU");
+    }
+    else if(op.b.op === 'arg'){
+        result.push(`AR ${op.b.n}`);
+        result.push("PU");
+    }     
+    else{
+           result.push(...func(op.b));
+    } 
+      result.push('PO');
+      result.push("SW");   
+      result.push('PO');
+      result.push(ins[op.op]);
+      result.push("PU");
+      return result;  
+  }
+return (function assembler(ast){
+             let asm =[];
+             if(ast && ast.op === 'imm')
+                    asm.push(`IM ${ast.n}`);
+             else if (ast && ast.op === 'arg') 
+                    asm.push(`AR ${ast.n}`)
+             else { 
+                    asm.push(...func(ast));
+                    // asm.push("PO");
+                    // asm.push("SW");   
+                    // asm.push("PO"); 
+                    // asm.push(ins[ast.op]); 
+                    asm.push('PO');
+              }
+              console.log(asm);
+             return asm;
+})(ast);
 };
-
 
 function unit(program,argsList) {
       var match, expr;
@@ -50,7 +118,7 @@ function unit(program,argsList) {
       else
         throw new SyntaxError("Unexpected syntax: " + program[0]);
       program.shift();
-      return factor(expr, program,argsList);
+      return expr;
 }
 function parse(program) {
       // var result = unit(program);
@@ -59,36 +127,37 @@ function parse(program) {
         throw new SyntaxError("Unexpected text after program");
       return result;
 }
-function factor(expr, program,argsList) {
+function factor(program,argsList) {
+      let expr = unit(program, argsList);
       if (expr.type != "delimiter")
-          return term(expr,program,argsList);
-      expr = expression(unit(program,argsList),program);
+          return expr;
+      expr = expression(term(factor(program, argsList),program,argsList),program,argsList);
        if (program[0] != ")")
           throw new SyntaxError("Expected  ')'");
        program.shift();
-       return term(expr, program,argsList);
+       return expr;
 }
-function term(expr,program,argsList){
+function term(f,program,argsList){
    if(program[0] !== "*" && program[0] !== "/" )
-         return expression(expr,program,argsList); 
-   expr = {op: program.shift() , a:expr, b:unit(program,argsList)}; 
-   return expression(expr,program,argsList);
+         return f;
+   let ex = {op: program.shift() , a:f, b:factor(program,argsList)}; 
+   return term(ex,program,argsList);
 }
-function expression(expr,program,argsList){
+function expression(t,program,argsList){
   if(program[0] !== "+" && program[0] !== "-" )
-         return expr; 
-   expr = {op: program.shift() , a:expr, b:unit(program,argsList)}; 
-   return expression(expr,program,argsList);
+         return t;
+  let expr = {op: program.shift() , a:t, b:term(factor(program, argsList),program,argsList)}; 
+  return expression(expr,program,argsList);
 }
 function functionDef(program){
    if(program[0] !== '['){
-      return unit(program);
+      return factor(program);
    }else{
       let argsList ={};
       let nth = 0;
       program.shift();
       while(program.length > 0 && program[0] !== ']'){
-          let arg = unit(program);
+          let arg = factor(program);
           if(arg.op !== 'arg')
                throw new SyntaxError(`Unexpected token ${arg.n}`);
           argsList[arg.n] = nth++; 
@@ -96,26 +165,10 @@ function functionDef(program){
       if(program[0] !== ']')
             throw new SyntaxError(`Invalid function definition`);
        program.shift();
-      return unit(program,argsList);
+      return expression(term(factor(program, argsList),program,argsList),program, argsList);
    } 
 }
-function evaluate(expr){
-  if(expr.type === 'value'){
-    return parseFloat(expr.value);
-  }else{
-      return reduce(expr.value);
-  }
-}
-function reduce(expr){
-  if(expr.length === 3)
-  switch(expr[1]){
-     case '+' : return evaluate(expr[0]) + evaluate(expr[2]);
-     case '-'  : return evaluate(expr[0]) - evaluate(expr[2]);
-     case '*' :  return evaluate(expr[0]) * evaluate(expr[2]);
-     case '/'  :  return evaluate(expr[0]) / evaluate(expr[2]);
-  }
-  return expr;
-}
+
 
 
 
